c3fb661889132ab05d191d54f41a77ee6f6a1b8e
Mikhael Kosminsky <kosminskym@hotmail.com>
2023-06-09 15:20:44 -0300

    add funcionalidade no botao de deletar comment

--- feed-reactJS/src/components/Comment.jsx
+++ feed-reactJS/src/components/Comment.jsx
@@ -3,7 +3,11 @@ import { ThumbsUp } from 'phosphor-react'
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+    function handleDeleteComment(){
+        onDeleteComment(content);
+    }
+
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://pps.whatsapp.net/v/t61.24694-24/351742681_255919237118240_3919252908336163091_n.jpg?ccb=11-4&oh=01_AdR9wa4x4nqyKS60oeLUoOoT3ueKIcU86ivElYGmg3J7_A&oe=648E099A" alt="" />
@@ -16,7 +20,7 @@ export function Comment({ content }) {
                             <time tittle="03 de Junho de 2023 às 10h20" dateTime="2023-06-03 11:13:00">Cerca de 1h atrás</time>
                         </div>
 
-                        <button tittle="Deletar comentário">
+                        <button onClick={handleDeleteComment} tittle="Deletar comentário">
                             <Trash size={24} />
                         </button>
                     </header>
